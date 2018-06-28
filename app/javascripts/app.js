c04206d151336310494f853f15286152db223617
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-06-29 00:40:17 +0200

    use infura as a fallback

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -121,13 +121,11 @@ window.App = {
 window.addEventListener('load', function() {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
   } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+    // fallback to infura
+    window.web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/nxqvLpMcFgty1XUFr67x"));
   }
 
   App.start();
