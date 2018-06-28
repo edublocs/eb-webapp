be9133682b2f0d48f4a5368e932b76b9245b8b1a
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-06-29 00:44:07 +0200

    don't complain about accounts when just displaying

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -24,24 +24,8 @@ window.App = {
     // Bootstrap the GradeBook abstraction for Use.
     GradeBook.setProvider(web3.currentProvider);
 
-    // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function(err, accs) {
-      if (err != null) {
-        alert("There was an error fetching your accounts.");
-        return;
-      }
-
-      if (accs.length == 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-        return;
-      }
-
-      accounts = accs;
-      account = accounts[0];
-
-      self.refreshEvaluationCount();
-      self.refreshStudents();
-    });
+    self.refreshEvaluationCount();
+    self.refreshStudents();
   },
 
   setStatus: function(message) {
