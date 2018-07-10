22243def312b535eda579261049fcf8b4d8363c9
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-07-10 17:44:12 +0200

    linting

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -20,7 +20,7 @@ var readOnly = false
 var students = []
 var previousEvaluationCount = 0
 
-function getQueryVariable(variable) {
+function getQueryVariable (variable) {
   var query = window.location.search.substring(1)
   var vars = query.split('&')
   for (var i = 0; i < vars.length; i++) {
